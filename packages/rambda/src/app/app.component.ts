1d923e26ed0e2e8a9ec97c99f23500ee8496868e
Dejan Toteff <deyan8284@gmail.com>
2020-06-21 01:40:53 +0300

    feat: add sample code

--- packages/rambda/src/app/app.component.ts
+++ packages/rambda/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { add, compose, dropLast, takeLast, range } from 'rambda'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rambda';
+
+  ngOnInit() {
+    const result = compose<any, any, any, any>(
+      x => x.map(add(7)),
+      takeLast(100),
+      dropLast(500)
+    )(range(1,1000))
+    console.log({result})
+  }
 }
